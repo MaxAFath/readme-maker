2254958ee6aa1a1c2eb0f0e7ba46dcc3856c2bb7
Max Fathauer <maxfathauer@gmail.com>
2022-04-17 13:45:15 -0400

    lost track of time commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["project name","What is your name?","What is your GitHub name?","What is the objective of this application?" ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
---
+++ src/page-template.js
@@ -0,0 +1,20 @@
+const generatepage = (projectName,projectDescription, projectGoal, credits, license) =>{
+    return `
+    #${projectName}
+    ##Description
+    
+    ${projectDescription}
+
+    #Usage
+    ${projectGoal}
+    
+    ##Credits
+    
+    ${credits}
+    
+    ##License
+    
+    ${License}`
+}
+
+module.exports = generatepage;
