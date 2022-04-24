b5b2749bfce8e90d9e0ff560383a67ebb917c95e
Max Fathauer <maxfathauer@gmail.com>
2022-04-24 11:52:41 -0400

    added questions to fill variables, breaking to replace oil pan on car

--- index.js
+++ index.js
@@ -1,30 +1,83 @@
-var fileName;
-var variables = [{
-    type:'input',
-    name:'name',
-    message:"What is the file's name?"
-},
-{
-    type:'input',
-    name:
-}
-];
+
+const { Console } = require('console');
 // TODO: Include packages needed for this application
 const fs = require('fs');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const asks = ["project name","What is your name?","What is your GitHub name?","What is the objective of this application?", "contributers"];
+const asks = ["project name",
+    "What is the objective of this application?",
+    'How to install',
+    'License',
+    'intended usage',
+    "contributering",
+    'testing instructions',
+    'gitHub',
+    'email',
+    ];
+const licenseArray = ['None','Apache License 2.0','GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause"Simplified" License', 'BSD 3-Clause"New" or "Revised" License', 'Bost Software License 1.0', 'Creative Commons Zero v1.0', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, variables) {
-    
+
 }
 
 // TODO: Create a function to initialize app
 function init() {
- 
-}
+    console.log(`
+=================
+Add a New Project
+=================
+    `);
+    const promptUser = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: fileName,
+                message: asks[0],
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    }
+                    else {
+                        console.log('Please eneter the file name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'descrition',
+                message: 'Provide a description of the project (Required)',
+                validate: descriptionInput => {
+                  if (descriptionInput) {
+                    return true;
+                  } else {
+                    console.log('You need to enter a project description!');
+                    return false;
+                  }
+                }
+            }, {
+                type: 'input',
+                name: 'install',
+                message: 'How do you install this application'
+                
+            }, {
+                type:'input',
+                name:'useage',
+                message:'How do you want people to use this application'
+            }, {
+                type:'list',
+                name:'license',
+                message:'What licenses are you using? (default is None)',
+                choices:licenseArray,
+                default:0
+            }, {
+
+            }
+        ]);
+    }
+};
 
 // Function call to initialize app
 init();
--- src/page-template.js
+++ src/page-template.js
@@ -1,12 +1,21 @@
-const generatepage = (projectName,projectDescription, projectGoal, credits, license) =>{
+const generatepage = (projectName,projectDescription, technology, credits, license) =>{
     return `
     #${projectName}
-    ##Description
-    
-    ${projectDescription}
 
-    ##Usage
-    ${projectGoal}
+    ##Description
+    ${projectDescription}
+    
+     #Tabel of contents
+    1.[Description](#description)
+    2.[Installation](#installation)
+    3.[Usage](#usage)
+    4.[Licsense](#license)
+    5.[Contributing](#contributing)
+    6.[Test Instructions](#testing)
+    7.[Questions](#questions)
+
+    ##Technology
+    ${technology}
     
     ##Credits
     
